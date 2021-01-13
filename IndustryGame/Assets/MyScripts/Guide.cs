0be60150482650280194ef1bfdd8d6dd7674778f
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-01-13 13:48:36 +0800

    add: guide wip

---
+++ IndustryGame/Assets/MyScripts/Guide.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guide : MonoBehaviour
+{
+    [TextArea]
+    public List<string> infos = new List<string>();
+    bool show = true;
+
+    private int currentIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(currentIndex)
+        {
+            case 0:
+                // judge if user clicked XXX
+                if (false)
+                {
+                    ++currentIndex;
+                    show = true;
+                }
+                break;
+            //TODO: judge currentIndex should increment and show should be true / false
+        }
+    }
+
+    public string currentInfo()
+    {
+        return currentIndex >= infos.Count ? null : infos[currentIndex];
+    }
+
+    public bool finished()
+    {
+        return currentInfo() == null;
+    }
+}
