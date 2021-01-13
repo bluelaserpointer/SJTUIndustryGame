1d3a34be50910f2777f8cacc989cc516e4a17904
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-01-13 13:29:07 +0800

    fix scripts

---
+++ IndustryGame/Assets/MyScripts/EventInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventInfo : MonoBehaviour
+{
+    public string infoName;
+    [TextArea]
+    public string description;
+    [TextArea]
+    public string descriptionAfterFinish;
+    public Dictionary<Ability, int> requiredAbilities = new Dictionary<Ability, int>();
+
+    private bool _isFinished = false;
+    public void finish()
+    {
+        _isFinished = true;
+    }
+    public bool isFinished()
+    {
+        return _isFinished;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
