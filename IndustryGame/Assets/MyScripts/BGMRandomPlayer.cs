16d53b90efe6c225a2b0247a0e48db311f5d8aa1
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-01-18 15:26:40 +0800

    apply AbilityIconProvider

---
+++ IndustryGame/Assets/MyScripts/BGMRandomPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Add ScriptableObjects/BGMRandomPlayer")]
+public class BGMRandomPlayer : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
