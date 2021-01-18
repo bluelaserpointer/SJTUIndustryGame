393b4d9e97f255e844ad1693b021fee87e6c8c6b
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-01-18 18:52:42 +0800

    add: BGMRandomPlayer

--- IndustryGame/Assets/MyScripts/BGMRandomPlayer.cs
+++ IndustryGame/Assets/MyScripts/BGMRandomPlayer.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Add ScriptableObjects/BGMRandomPlayer")]
 public class BGMRandomPlayer : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public AudioSource audioSource;
+    public List<AudioClip> clips;
+
+    private int clipIndex;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (clips.Count > 0)
+            return;
+        if(!audioSource.isPlaying)
+        {
+            audioSource.clip = clips[clipIndex = Random.Range(0, clips.Count)];
+            audioSource.Play();
+        }
     }
 }
