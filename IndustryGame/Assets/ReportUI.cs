d764537fa643270451db3ca7f17f9365fc14a3d4
Naomi <41462684+naomixie@users.noreply.github.com>
2021-01-19 11:23:27 +0800

    Revert "Revert "UI:wip""
    This reverts commit 2ed6cb5831813e6fb02c77571194d106b3f74e9a.

---
+++ IndustryGame/Assets/ReportUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReportUI : MonoBehaviour
+{
+    public GameObject EventReportList;
+    public GameObject AnimalReportList;
+    public GameObject EnvironmentReportList;
+    public GameObject SingleEventReportPrefab;      //报告Prefab，三个都应该是一样的
+    private List<Event> events;
+    private List<EventInfo> eventinfos;
+    
+    void Start()
+    {
+        events = Stage.GetEvents();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //void InstantiateSpecialistList ()
+    //{
+    //    for (int i = 0 ; i < specialists.Count ; i++)
+    //    {
+    //        GameObject clone;
+    //        clone = Instantiate(SingleSpecialistPrefab, SpecialistList.transform, false);
+    //        clone.GetComponent<SingleSpecialist>().specialist = specialists[i];
+    //    }
+    //}
+}
