15bd41535216f1677917fbadb92f5faebc43887f
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-01-19 19:55:27 +0800

    wip: protoype of PopUpWindow

--- IndustryGame/Assets/MyScripts/PopUpWindow.cs
+++ IndustryGame/Assets/MyScripts/PopUpWindow.cs
@@ -4,16 +4,11 @@ using UnityEngine;
 
 public class PopUpWindow : MonoBehaviour
 {
-    private static PopUpWindow staticInstance;
-    public void Awake()
+    public string title;
+    public string contents;
+
+    public void show()
     {
-        if(staticInstance == null)
-        {
-            staticInstance = this;
-        }
-    }
-    public static void show()
-    {
-        staticInstance.transform.gameObject.SetActive(true);
+        transform.gameObject.SetActive(true);
     }
 }
