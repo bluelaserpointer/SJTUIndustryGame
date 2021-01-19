b32728e4a8b5e586754f966536f0528ac4e6c04e
Naomi <41462684+naomixie@users.noreply.github.com>
2021-01-19 16:11:25 +0800

    UI: 事件报告

---
+++ IndustryGame/Assets/EventReportUI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventReportUI : MonoBehaviour
+{
+    public Event eventDetails;
+    public Text EventDescription;
+    public Text EventDescriptionAfterFinish;
+
+    public GameObject SingleEventInfoPrefab;
+    public GameObject EventInfoList;
+    
+    void Start()
+    {
+        InstantiateEventInfoList();
+    }
+
+    void Update()
+    {
+        EventDescription.text = eventDetails.description;
+    }
+
+    private void OnEnable ()
+    {
+        InstantiateEventInfoList();
+    }
+
+    public void InstantiateEventInfoList ()
+    {
+        for (int i = 0 ; i < eventDetails.includedInfos.Count ; i++)
+        {
+            GameObject clone = Instantiate(SingleEventInfoPrefab, EventInfoList.transform, false);
+            clone.GetComponent<EventInfoUI>().eventInfo = eventDetails.includedInfos[i];
+            InGameLog.AddLog(eventDetails.includedInfos[i].infoName);
+
+        }
+    }
+
+    public void CloseWindow ()
+    {
+        Destroy(gameObject);
+    }
+}
