50658a3bcc2efc6f1b15dd8bbbf3274babcec934
Naomi <41462684+naomixie@users.noreply.github.com>
2021-01-20 23:13:25 +0800

    UI: wip areaDetails

---
+++ IndustryGame/Assets/AreaDetailsHUD.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class AreaDetailsHUD : MonoBehaviour
+{
+    private Area CurrentArea;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        CurrentArea = OrthographicCamera.GetMousePointingArea();    
+    }
+
+}
