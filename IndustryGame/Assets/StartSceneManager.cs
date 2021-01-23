675174b16052a6f056952ab596fba4462b5dce92
Naomi <41462684+naomixie@users.noreply.github.com>
2021-01-23 23:20:08 +0800

    feat: <UI> 添加StartScene

---
+++ IndustryGame/Assets/StartSceneManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : MonoBehaviour
+{
+    public void EnterGame ()
+    {
+        SceneManager.LoadScene("UI");
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
