aeac7b428a07d0ceaa46e56b18e802438d356e9d
Naomi <41462684+naomixie@users.noreply.github.com>
2021-01-24 12:14:40 +0800

    feat: <UI> 提示框

--- IndustryGame/Assets/MyScripts/UI/SimplePopUpWindow.cs
+++ IndustryGame/Assets/MyScripts/UI/SimplePopUpWindow.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class SimplePopUpWindow : IPopUpWindow
 {
-    public string title;
-    public string contents;
+    private string title;
+    private string contents;
+    private Sprite picture;
 
-    public SimplePopUpWindow (string title, string contents)
+    public SimplePopUpWindow (string title, string contents /*, Sprite picture */)
     {
         this.title = title;
         this.contents = contents;
+        //this.picture = picture;
     }
 
     public void Generate ()
     {
         GameObject clone = GameObject.Instantiate(PopUpCanvas.instance.SinglePopUpWindowPrefab, PopUpCanvas.instance.transform, false);
-        clone.GetComponent<SinglePopUpWindow>().title = title;
-        clone.GetComponent<SinglePopUpWindow>().contents = contents;
+        clone.GetComponent<SinglePopUpWindow>().TitleText.text = title;
+        clone.GetComponent<SinglePopUpWindow>().ContentsText.text = contents;
+        //clone.GetComponent<SinglePopUpWindow>().Picture.sprite = picture;
     }
 }
