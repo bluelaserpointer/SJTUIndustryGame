74e28af0c8d74f67732ef7e9f1080127371ea3cc
Naomi <41462684+naomixie@users.noreply.github.com>
2021-01-24 12:30:04 +0800

    feat: <UI> 将图片提示框与无图片提示框区分开来
    - 图片提示框使用PicturePopUpWindow

--- IndustryGame/Assets/MyScripts/UI/PicturePopUpWindow.cs
+++ IndustryGame/Assets/MyScripts/UI/PicturePopUpWindow.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SimplePopUpWindow : IPopUpWindow
+public class PicturePopUpWindow : IPopUpWindow
 {
     private string title;
     private string contents;
     private Sprite picture;
 
-    public SimplePopUpWindow (string title, string contents)
+    public PicturePopUpWindow (string title, string contents , Sprite picture )
     {
         this.title = title;
         this.contents = contents;
+        this.picture = picture;
     }
 
     public void Generate ()
     {
-        GameObject clone = GameObject.Instantiate(PopUpCanvas.instance.SimplePopUpWindowPrefab, PopUpCanvas.instance.transform, false);
+        GameObject clone = GameObject.Instantiate(PopUpCanvas.instance.PicturePopUpWindowPrefab, PopUpCanvas.instance.transform, false);
         clone.GetComponent<SinglePopUpWindow>().TitleText.text = title;
         clone.GetComponent<SinglePopUpWindow>().ContentsText.text = contents;
+        clone.GetComponent<SinglePopUpWindow>().Picture.sprite = picture;
     }
 }
