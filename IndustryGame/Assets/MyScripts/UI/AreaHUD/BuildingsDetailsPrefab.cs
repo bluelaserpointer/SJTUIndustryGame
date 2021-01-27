a9cc697fc78ed841cbeba4adc03b41f4ff1bccda
Naomi <41462684+naomixie@users.noreply.github.com>
2021-01-27 15:32:34 +0800

    feat: <UI> 地图详情页现在可以查看更多信息
    - 可以查看已执行措施
    - 可以查看可以执行措施
    - 可以查看已有建筑物

---
+++ IndustryGame/Assets/MyScripts/UI/AreaHUD/BuildingsDetailsPrefab.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingsDetailsPrefab : MonoBehaviour
+{
+    private Building building;
+    public Text buildingName;
+
+    public void RefreshUI(Building building)
+    {
+        this.building = building;
+        buildingName.text = building.info.buildingName;
+    }
+
+}
