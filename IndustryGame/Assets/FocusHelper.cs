992d7288942289501270e3ebb5a39a84e0dcd359
Naomi <41462684+naomixie@users.noreply.github.com>
2021-02-14 05:46:14 +0800

    fix: 基本需求

---
+++ IndustryGame/Assets/FocusHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FocusHelper : MonoBehaviour
+{
+    public Color SelectedColor;
+    public Color NormalColor;
+
+
+    private Image SelectedButton;
+    public Image StartSelectedButton;
+
+    void Start()
+    {
+        SelectImage(StartSelectedButton);
+    }
+
+    public void SelectImage(Image SelectButton)
+    {
+        ClearSelection();
+        SelectedButton = SelectButton;
+        SelectedButton.color = SelectedColor;
+    }
+
+    public void ClearSelection()
+    {
+        if (SelectedButton != null)
+        {
+            SelectedButton.color = NormalColor;
+        }
+    }
+}
