c453d0f61743751282f7c4efe568f936c5ca75cb
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-02-14 16:51:06 +0800

    wip: animal number display system

--- IndustryGame/Assets/MyScripts/Tool/SimpleBehaviour/LookAtMainCamera.cs
+++ IndustryGame/Assets/MyScripts/Tool/SimpleBehaviour/LookAtMainCamera.cs
@@ -12,7 +12,8 @@ public class LookAtMainCamera : MonoBehaviour
             GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
             if (mainCamera == null)
                 return;
-            transform.rotation = Quaternion.LookRotation(new Vector3(0, mainCamera.transform.position.y - transform.position.y, Mathf.Abs(mainCamera.transform.position.z - transform.position.z)));
+            //transform.rotation = Quaternion.LookRotation(new Vector3(0, mainCamera.transform.position.y - transform.position.y, Mathf.Abs(mainCamera.transform.position.z - transform.position.z)));
+            transform.rotation = mainCamera.transform.rotation;
         }
     }
 }
