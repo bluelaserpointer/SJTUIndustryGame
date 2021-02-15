de8f4fbda443ec523a0958dc5db6a04588c1dd8e
Naomi <41462684+naomixie@users.noreply.github.com>
2021-02-15 21:27:47 +0800

    wip: Animal FilterPanel

---
+++ IndustryGame/Assets/AnimalSelection.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalSelection : MonoBehaviour
+{
+    [HideInInspector]
+    public Animal animal;
+    [HideInInspector]
+    public Button button;
+
+    public Text AnimalName; //TODO: replace with picture;
+
+    void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(() => Stage.ShowAnimalNumberPop(animal));
+        // button.onClick.AddListener(() => FilterPanel.instance.)
+    }
+
+    void Update()
+    {
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        AnimalName.text = animal.animalName;
+    }
+}
---
+++ IndustryGame/Assets/FilterPanelAnimalPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilterPanelAnimalPanel : MonoBehaviour
+{
+    public MainEvent mainEvent;
+    public GameObject AnimalSelectionPrefab;
+
+    public GameObject AnimalSelectionGenerationPosition;
+
+    private List<GameObject> GeneratedAnimalSelectionPrefabs = new List<GameObject>();
+
+    public void RefreshUI()
+    {
+        Helper.ClearList(GeneratedAnimalSelectionPrefabs);
+        foreach (Animal animal in mainEvent.concernedAnimals)
+        {
+            GameObject clone = Instantiate(AnimalSelectionPrefab, AnimalSelectionGenerationPosition.transform, false);
+            clone.GetComponent<AnimalSelection>().animal = animal;
+            GeneratedAnimalSelectionPrefabs.Add(clone);
+        }
+    }
+}
