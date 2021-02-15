12ab3600372b29e22d894cac084f2c3e77d971ee
Yuu-233 <samsara@sjtu.edu.cn>
2021-02-16 02:54:32 +0800

    feat: add path animating

--- IndustryGame/Assets/MyScripts/MapScripts/HexUnit.cs
+++ IndustryGame/Assets/MyScripts/MapScripts/HexUnit.cs
@@ -1,8 +1,14 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 using System.IO;
 public class HexUnit : MonoBehaviour
 {
 	public static HexUnit unitPrefab;
+	List<HexCell> pathToTravel;
+	const float travelSpeed = 4f;
+	const float rotationSpeed = 180f;
+
 	public HexCell Location
 	{
 		get
@@ -35,7 +41,13 @@ public class HexUnit : MonoBehaviour
 
 	float orientation;
 	HexCell location;
-
+	void OnEnable()
+	{
+		if (location)
+		{
+			transform.localPosition = location.Position;
+		}
+	}
 	public void ValidateLocation()
 	{
 		transform.localPosition = location.Position;
@@ -63,4 +75,76 @@ public class HexUnit : MonoBehaviour
 	{
 		return !cell.IsUnderwater && !cell.Unit;
 	}
+	public void Travel(List<HexCell> path)
+	{
+		Location = path[path.Count - 1];
+		pathToTravel = path;
+		StopAllCoroutines();
+		StartCoroutine(TravelPath());
+	}
+
+
+	IEnumerator TravelPath()
+	{
+		Vector3 a, b, c = pathToTravel[0].Position;
+		transform.localPosition = c;
+		yield return LookAt(pathToTravel[1].Position);
+
+		float t = Time.deltaTime * travelSpeed;
+		for (int i = 1; i < pathToTravel.Count; i++)
+		{
+			a = c;
+			b = pathToTravel[i - 1].Position;
+			c = (b + pathToTravel[i].Position) * 0.5f;
+			for (; t < 1f; t += Time.deltaTime * travelSpeed)
+			{
+				transform.localPosition = Bezier.GetPoint(a, b, c, t);
+				Vector3 d = Bezier.GetDerivative(a, b, c, t);
+				d.y = 0f;
+				transform.localRotation = Quaternion.LookRotation(d);
+				yield return null;
+			}
+			t -= 1f;
+		}
+
+		a = c;
+		b = pathToTravel[pathToTravel.Count - 1].Position;
+		c = b;
+		for (; t < 1f; t += Time.deltaTime * travelSpeed)
+		{
+			transform.localPosition = Bezier.GetPoint(a, b, c, t);
+			Vector3 d = Bezier.GetDerivative(a, b, c, t);
+			d.y = 0f;
+			transform.localRotation = Quaternion.LookRotation(d);
+			yield return null;
+		}
+		transform.localPosition = location.Position;
+		orientation = transform.localRotation.eulerAngles.y;
+		ListPool<HexCell>.Add(pathToTravel);
+		pathToTravel = null;
+	}
+
+	IEnumerator LookAt(Vector3 point)
+	{
+		point.y = transform.localPosition.y;
+		Quaternion fromRotation = transform.localRotation;
+		Quaternion toRotation =
+			Quaternion.LookRotation(point - transform.localPosition);
+		float angle = Quaternion.Angle(fromRotation, toRotation);
+		if (angle > 0f)
+		{
+			float speed = rotationSpeed / angle;
+
+			for (float t = Time.deltaTime * speed; t < 1f; t += Time.deltaTime * speed)
+			{
+				transform.localRotation =
+					Quaternion.Slerp(fromRotation, toRotation, t);
+				yield return null;
+			}
+		}
+		transform.LookAt(point);
+		orientation = transform.localRotation.eulerAngles.y;
+			
+	}
 }
+
