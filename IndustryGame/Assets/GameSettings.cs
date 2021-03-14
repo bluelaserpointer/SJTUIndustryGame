e94acde09a21535cb80b83510d397a8e078f7ada
Naomi <41462684+naomixie@users.noreply.github.com>
2021-03-14 17:41:34 +0800

    feat: mouse click sound

---
+++ IndustryGame/Assets/GameSettings.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings : MonoBehaviour
+{
+    public static GameSettings instance;
+    public float volume = 0.1f;
+    public AudioClip MouseClickClip;
+    private AudioSource ClickAudioSource;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        ClickAudioSource = gameObject.GetComponent<AudioSource>();
+        ClickAudioSource.clip = MouseClickClip;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ClickAudioSource.Play();
+        }
+    }
+
+}
