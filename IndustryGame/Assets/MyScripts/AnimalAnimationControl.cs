062890ce2c603319d74498613dbc73e8b284a4aa
edwardraymondhe <43778680+edwardraymondhe@users.noreply.github.com>
2021-04-06 03:23:27 +0800

    feat: added animation random start time control

---
+++ IndustryGame/Assets/MyScripts/AnimalAnimationControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalAnimationControl : MonoBehaviour
+{
+    private Animator animator;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Random.Range(0.0f, 5.0f);
+
+        // Sets the animator off
+        animator = GetComponent<Animator>();
+        if(animator != null)
+            StartCoroutine(AnimationStart());
+    }
+
+    IEnumerator AnimationStart()
+    {
+        yield return new WaitForSeconds(startTime);
+        
+        animator.enabled = true;
+    }
+}
