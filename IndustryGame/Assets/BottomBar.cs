9864e71b8142870d2b1355f933da29d33fbd1666
Naomi <41462684+naomixie@users.noreply.github.com>
2021-04-15 00:44:35 +0800

    fix: BottomBar现在能知道哪一个被选中

--- IndustryGame/Assets/BottomBar.cs
+++ IndustryGame/Assets/BottomBar.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BottomBar : MonoBehaviour
 {
     public static BottomBar instance;
+    public Button BuildingsButton;
+    public Button SpecialistButton;
+
+    public Color TabChosenColor;
+    public Color TabUnchosenColor;
 
     void Awake()
     {
@@ -17,17 +23,24 @@ public class BottomBar : MonoBehaviour
     void Start()
     {
         ActivateSpecialistBar();
+        SpecialistButton.onClick.AddListener(() => ActivateSpecialistBar());
+        BuildingsButton.onClick.AddListener(() => ActivateBuildingsBar());
     }
 
     public void ActivateSpecialistBar()
     {
         SpecialistBar.instance.gameObject.SetActive(true);
         BuildingsBar.instance.gameObject.SetActive(false);
+        SpecialistButton.image.color = TabChosenColor;
+        BuildingsButton.image.color = TabUnchosenColor;
+
     }
 
     public void ActivateBuildingsBar()
     {
         SpecialistBar.instance.gameObject.SetActive(false);
         BuildingsBar.instance.gameObject.SetActive(true);
+        SpecialistButton.image.color = TabUnchosenColor;
+        BuildingsButton.image.color = TabChosenColor;
     }
 }
