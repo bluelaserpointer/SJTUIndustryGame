8c27da7427d45c34636ba952754e4e9432a47553
Yuu-233 <samsara@sjtu.edu.cn>
2021-04-15 21:56:21 +0800

    feat : add book scene to game

---
+++ IndustryGame/Assets/MyScripts/BookScene/BookSceneManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BookSceneManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioClip FlippingClip;
+    public Sprite rightNext;
+    void Start()
+    {
+        rightNext = GameObject.Find("RightNext").GetComponent<Image>().sprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayFlippingSound()
+    {
+        //AudioSource.PlayClipAtPoint(FlippingClip, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        rightNext = GameObject.Find("RightNext").GetComponent<Image>().sprite;
+        if (rightNext.name == "TransparentGraybackgtound")
+        {
+            Invoke("StartGame", 0.01f);
+        }
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("UI");
+    }
+
+}
