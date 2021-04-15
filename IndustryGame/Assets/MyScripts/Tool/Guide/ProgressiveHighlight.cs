cb3befc1ad6c7743094a12f873b150db19ecb43b
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2021-04-15 13:47:55 +0800

    wip: progressive UI highlight guidance

--- IndustryGame/Assets/MyScripts/Tool/Guide/ProgressiveHighlight.cs
+++ IndustryGame/Assets/MyScripts/Tool/Guide/ProgressiveHighlight.cs
@@ -7,26 +7,34 @@ using UnityEngine.UI;
 [DisallowMultipleComponent]
 public class ProgressiveHighlight : MonoBehaviour
 {
+    public string staticName;
     public GameObject highlightPrefab;
     [SerializeField]
-    private Button button;
+    private List<Button> buttons;
     [SerializeField]
     private bool highlightOnAwake;
     private bool isHighlighted;
-    [SerializeField]
-    private List<ProgressiveHighlight> nextHighlights;
-    public UnityEvent highlighted;
+    public List<ProgressiveHighlight> nextHighlights;
+    public UnityEvent highlightEvent;
+    public UnityEvent stopHighlightEvent;
 
     //data
+    private static List<ProgressiveHighlight> staticInstances = new List<ProgressiveHighlight>();
     public bool IsHighlighted => isHighlighted;
     private GameObject generatedHighlight;
     private List<ProgressiveHighlight> eraseHighlightsAfterClick = new List<ProgressiveHighlight>();
 
+    static ProgressiveHighlight()
+    {
+        staticInstances.Clear();
+    }
     private void Awake()
     {
+        if(staticName.Length > 0)
+            staticInstances.Add(this);
         if (highlightOnAwake)
             Highlight();
-        button.onClick.AddListener(Click);
+        buttons.ForEach(eachButton => eachButton.onClick.AddListener(Click));
     }
     public void Click()
     {
@@ -52,6 +60,7 @@ public class ProgressiveHighlight : MonoBehaviour
         {
             isHighlighted = true;
             generatedHighlight = Instantiate(highlightPrefab, transform);
+            highlightEvent.Invoke();
         }
     }
     public void StopHighlight()
@@ -59,6 +68,7 @@ public class ProgressiveHighlight : MonoBehaviour
         if(isHighlighted)
         {
             isHighlighted = false;
+            stopHighlightEvent.Invoke();
             Destroy(generatedHighlight);
         }
     }
---
+++ IndustryGame/Assets/MyScripts/Tool/Guide/ProgressiveHighlightStarterECO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressiveHighlightStarterECO : MonoBehaviour
+{
+    public EventStageSO eventStageSO;
+    [Serializable]
+    public struct HighlightAndText
+    {
+        public ProgressiveHighlight highlightTarget;
+        public string guideText;
+    }
+    public List<HighlightAndText> highlightAndTexts;
+    public void Start()
+    {
+        eventStageSO.appearEvent.AddListener(Invoke);
+        eventStageSO.finishEvent.AddListener(End);
+    }
+    public void Invoke()
+    {
+        if (highlightAndTexts.Count == 0)
+            return;
+        //highlight first one
+        if(highlightAndTexts[0].highlightTarget != null)
+            highlightAndTexts[0].highlightTarget.Highlight();
+        for(int i = 0; i < highlightAndTexts.Count; ++i)
+        {
+            if (i != highlightAndTexts.Count - 1)
+            {
+                ProgressiveHighlight highlightTarget = highlightAndTexts[i].highlightTarget;
+                string guideText = highlightAndTexts[i].guideText;
+                highlightTarget.nextHighlights.Clear();
+                if(highlightAndTexts[i + 1].highlightTarget != null)
+                    highlightTarget.nextHighlights.Add(highlightAndTexts[i + 1].highlightTarget);
+                highlightTarget.highlightEvent.RemoveAllListeners();
+                highlightTarget.highlightEvent.AddListener(() => GuideTextDisplay.instance.AddGuideLine("下一步操作", guideText));
+                highlightTarget.stopHighlightEvent.RemoveAllListeners();
+                highlightTarget.stopHighlightEvent.AddListener(() => GuideTextDisplay.instance.RemoveGuideLine("下一步操作", guideText));
+            }
+        }
+    }
+    public void End()
+    {
+        foreach(HighlightAndText pair in highlightAndTexts)
+        {
+            if(pair.highlightTarget != null)
+                pair.highlightTarget.StopHighlight();
+        }
+    }
+}
